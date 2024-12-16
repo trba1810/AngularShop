3ad079863d657ef25cac10c94948995477a6f028
Tomislav <trba8l@live.com>
2024-12-16 19:42:29 +0100

    improve header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   template: `
-    <div class="bg-slate-600 px-4 py-3 shadow-md"><span>My Web Shop</span></div>
+    <div
+      class="bg-slate-600 px-4 py-3 shadow-md flex justify-between items-center"
+    >
+      <span class="text-xl text-white">My Web Shop</span>
+      <app-primary-button label="Cart" (btnClicked)="showBtnClicked()" />
+    </div>
   `,
   styles: `
   `,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  showBtnClicked() {
+    console.log('clicked');
+  }
+}
---
+++ src/app/components/primary-button/primary-button.component.ts
@@ -0,0 +1,20 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-button',
+  imports: [],
+  template: `
+    <button
+      class="bg-blue-500 text-white w-full border px-5 py-2 rounded-xl shadow-md hover:opacity-80"
+      (click)="btnClicked.emit()"
+    >
+      {{ label() }}
+    </button>
+  `,
+  styles: ``,
+})
+export class PrimaryButtonComponent {
+  label = input('');
+
+  btnClicked = output();
+}
