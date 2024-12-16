d93e9b9cdbe9d6ef919a24200e5ee86fd9cd6f3f
Tomislav <trba8l@live.com>
2024-12-16 19:23:39 +0100

    making header and installing packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  imports: [HeaderComponent],
+  template: ` <app-header /> `,
   styles: [],
 })
 export class AppComponent {
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  template: `
+    <div class="bg-slate-600 px-4 py-3 shadow-md"><span>My Web Shop</span></div>
+  `,
+  styles: `
+  `,
+})
+export class HeaderComponent {}
